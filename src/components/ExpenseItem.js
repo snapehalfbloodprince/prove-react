363dfe43a622b74a9d73dc30cb5e98a6626a546d
Francesco Paolo Piga <fppiga@outlook.com>
2021-12-30 18:01:33 +0100

    Aggiunto il passaggio di valori tramite props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,12 @@
 import './ExpenseItem.css';
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28); //i mesi cominciano da 0, dunque 2 è marzo.
-  const expenseTitle = 'Assicurazione Auto';
-  const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
+  
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toDateString()}</div>
+      <div>{props.date.toDateString()}</div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
